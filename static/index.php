f0bc60395fffe9a7992be1fd276cef75edf078f8
lstephensca <lstephens@chumneyads.com>
2020-06-01 10:59:49 -0400

    first commit

---
+++ static/index.php
@@ -0,0 +1,295 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>C&S Natureworks</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="dist/css/bulma.css">
+        <link rel="stylesheet" href="dist/css/slick.css">
+        <link rel="stylesheet" href="dist/css/slick-theme.css">
+        <link rel="stylesheet" href="dist/css/main.css">
+    </head>
+    <body>
+        
+
+    <section>
+
+        <nav class="navbar" role="navigation" aria-label="main navigation">
+            
+            <div class="navbar-brand">
+                <a class="navbar-item is-size-3 has-text-primary" href="#">C&S Natureworks</a>
+                <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+                    <span aria-hidden="true"></span>
+                    <span aria-hidden="true"></span>
+                    <span aria-hidden="true"></span>
+                </a>
+            </div>
+
+            <div id="navbarBasicExample" class="navbar-menu">
+                <div class="navbar-end">
+                    <a class="navbar-item">Home</a>
+                    <div class="navbar-item has-dropdown is-hoverable">
+                        <a class="navbar-link">Services</a>
+                        <div class="navbar-dropdown">
+                            <a class="navbar-item">Land Clearing</a>
+                            <a class="navbar-item">Forestry Mulching</a>
+                            <a class="navbar-item">Brush Cutting</a>
+                            <a class="navbar-item">Grading</a>
+                            <a class="navbar-item">Ponds</a>
+                            <a class="navbar-item">Residential Handscape</a>
+                            <a class="navbar-item">Wildlife Removal</a>
+                        </div>
+                    </div>
+                    <a class="navbar-item">Gallery</a>
+                    <a class="navbar-item">About Us</a>
+                    <a class="navbar-item">Contact Us</a>
+                </div>
+                
+            </div>
+        </nav>
+
+    </section>
+
+    <!-- services -->
+    <section id="services">
+        <div class="container">
+            <div class="columns">
+                <div class="column is-12-desktop is-12-mobile has-text-centered">
+                    <h2 class="is-size-3">Our Services</h2>
+                </div>
+            </div>
+
+
+            <div class="columns">
+                
+                <div class="column is-4-desktop is-12-mobile">
+
+                    <div class="card has-background-primary has-text-white">
+                        <div class="card-image">
+                            <figure class="image is-4by3">
+                                <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder">
+                            </figure>
+                        </div>
+                        <div class="card-content">
+                            <div class="content">
+                            <h2 class="is-size-4">Land Management</h2>
+                                <p>Nulla vitae elit libero, a pharetra augue. Maecenas sed diam eget risus varius blandit sit amet non magna. Sed posuere consectetur est at lobortis. Maecenas faucibus mollis interdum.</p>
+                            </div>
+                        </div>
+                        <a href="#" class="button is-secondary service-btn">> Click for more info</a>
+                    </div>
+
+                </div>
+
+
+                <div class="column is-4-desktop is-12-mobile">
+
+                    <div class="card has-background-secondary has-text-white">
+                        <div class="card-image">
+                            <figure class="image is-4by3">
+                                <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder">
+                            </figure>
+                        </div>
+                        <div class="card-content">
+                            <div class="content">
+                                <h2 class="is-size-4">Wildlife Removal</h2>
+                                <p>Nulla vitae elit libero, a pharetra augue. Maecenas sed diam eget risus varius blandit sit amet non magna. Sed posuere consectetur est at lobortis. Maecenas faucibus mollis interdum.</p>
+                            </div>
+                        </div>
+                        <a href="#" class="button is-primary service-btn">> Click for more info</a>
+                    </div>
+
+                </div>
+
+
+                <div class="column is-4-desktop is-12-mobile">
+
+                    <div class="card has-background-primary has-text-white">
+                        <div class="card-image">
+                            <figure class="image is-4by3">
+                                <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder">
+                            </figure>
+                        </div>
+                        <div class="card-content">
+                            <div class="content">
+                                <h2 class="is-size-4">Pond & Water Features</h2>
+                                <p>Nulla vitae elit libero, a pharetra augue. Maecenas sed diam eget risus varius blandit sit amet non magna. Sed posuere consectetur est at lobortis. Maecenas faucibus mollis interdum.</p>
+                            </div>
+                        </div>
+                        <a href="#" class="button is-secondary service-btn">> Click for more info</a>
+                    </div>
+
+                </div>
+
+
+
+
+            </div>
+        </div>
+    </section>
+    <!-- services -->
+
+
+
+    <!-- main -->
+    <section id="about">
+        <div class="container">
+            <div class="columns">
+
+                <div class="column is-7-desktop">
+                    <h1 class="is-size-2 has-text-secondary">Welcome To <span class="has-text-primary">C & S Natureworks</span></h1>
+                
+                    <p>C & S Nature Works is the best contractor for forestry mulching, land clearing, brush clearing, and landscaping services – such as grading, brush cutting, and ponds – in Upstate SC. Our land management services take care of residential, commercial, and industrial outdoor needs in Greenville, Spartanburg, Anderson, Pickens, Laurens, and Cherokee counties in South Carolina.</p>
+
+                    <p>Beautify – Let C & S Nature Works transform your property by clearing overgrown brush or installing residential hardscape or water features.
+                    <p>Reclaim – We can help you find your land’s full potential by opening up unusable areas for farming, access roads, construction, and more.
+                    <p>Protect – Keep your home, crops, and loved ones safe from land erosion, fire hazards, and harmful wildlife.
+
+                    <p>We handle projects from beginning to finish – from clearing services such as excavation, debris removal, and landscape prep to appearance upgrades like ponds, patios, and fire pits. If your job involves the outdoors, it’s our specialty.
+                </div>
+
+                <div class="column is-5-desktop">
+                    <iframe width="100%" height="315" src="https://www.youtube.com/embed/UJ6axmLoQZ8?rel=0" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+                </div>
+
+
+
+            </div>
+
+        </div>
+
+    </section>
+    <!-- main -->
+
+    <!-- steps -->
+        <section id="steps">
+            <div class="container">
+                <div class="columns has-text-white">
+                        <div class="column is-desktop-7">
+                            <div class="step">
+                                <p class="number">1</p>
+                                <h3>Experience</h3>
+                                <p>Maecenas sed diam eget risus varius blandit sit amet non magna. Donec ullamcorper nulla non metus auctor fringilla. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Cras mattis consectetur purus sit amet fermentum. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Integer posuere erat a ante venenatis dapibus posuere velit aliquet.</p>
+                            </div>
+                            <div class="step">
+                                <p class="number">2</p>
+                                <h3>Quality</h3>
+                                <p>Maecenas sed diam eget risus varius blandit sit amet non magna. Donec ullamcorper nulla non metus auctor fringilla. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Cras mattis consectetur purus sit amet fermentum. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Integer posuere erat a ante venenatis dapibus posuere velit aliquet.</p>
+                            </div>
+
+                            <div class="step">
+                                <p class="number">3</p>
+                                <h3>Satisfaction</h3>
+                                <p>Maecenas sed diam eget risus varius blandit sit amet non magna. Donec ullamcorper nulla non metus auctor fringilla. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Cras mattis consectetur purus sit amet fermentum. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Integer posuere erat a ante venenatis dapibus posuere velit aliquet.</p>
+                            </div>
+
+                        </div>
+
+                        <div class="column is-desktop-5">
+                                <img src="dist/images/steps.png" alt="steps" />
+                        </div>
+
+                </div>
+            </div>
+        </section>
+
+
+    <!-- steps -->
+
+    <!-- testimonials -->
+        <section id="testimonials">
+            <div class="container">
+                <div class="columns">
+                    <div class="column is-desktop-12">
+
+                        <div class="slider test">
+
+                            <div class="testimonial">
+                                <p>Maecenas sed diam eget risus varius blandit sit amet non magna.</p>
+                            </div>
+
+
+
+                        </div>
+                        
+
+
+
+                    </div>
+                </div>
+            </div>
+        </section>
+
+    <!-- testimonials -->
+
+    <!-- section -->
+    <section>
+        <div class="container">
+            <div class="columns is-centered">
+                <div class="column is-desktop-4"><p>C & S Natureworks</p></div>
+                <div class="column is-desktop-4"><p>Column</p></div>
+                <div class="column is-desktop-4">
+                    <ul>
+                        <li>(864) 415-7979</li>
+                        <li>csnatureworks@gmail.com</li>
+                        <li>Like Us on Facebook</li>
+                    </ul>    
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+
+
+
+
+    <!-- section -->
+
+
+
+
+
+
+    <!-- footer -->
+    <footer class="footer">
+        <div class="content">
+            <?php $currentYear = date('Y'); ?>
+            <p>&copy; <?= $currentYear; ?> Chumney & Associates</p>
+        </div>
+    </footer>
+    <!-- footer -->
+
+
+    <script src="https://www.theautohost.com/_assets/coding/js/jquery-2.1.4.js"></script>
+    <script type="text/javascript" src="dist/js/slick.min.js"></script>
+    <script type="text/javascript" src="lib/main.js"></script>
+    
+    <script type="text/javascript">
+        $(document).ready(function() {
+            // Check for click events on the navbar burger icon
+            $(".navbar-burger").click(function() {
+              // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+                $(".navbar-burger").toggleClass("is-active");
+                $(".navbar-menu").toggleClass("is-active");
+            });
+
+            $('.test').slick({
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                arrows: true,
+                infinite: true,
+                autoplay: true,
+
+            });
+
+
+        });
+    
+    </script>
+    
+    
+    </body>
+</html>
